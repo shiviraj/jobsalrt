d84d9e1329a0589819f7017beeb4ace833c05055
shiviraj <58025059+shiviraj@users.noreply.github.com>
2020-11-14 11:14:34 +0000

    Frontend: |#9|added material ui as dependency

--- public/precache-manifest.bde6d9e6d7b928fd77ac07011ce02b4e.js
+++ public/precache-manifest.bde6d9e6d7b928fd77ac07011ce02b4e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "df484e1bcb8157dd2b8cf6e2d7ed70b3",
+    "revision": "a83a460d61567cdd7f2290b82ab699d7",
     "url": "/index.html"
   },
   {
-    "revision": "7452b2465f3fa57ac34f",
+    "revision": "8944b2d7319575adf4f2",
     "url": "/static/css/main.a0082d4f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.29fc5fc1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7452b2465f3fa57ac34f",
-    "url": "/static/js/main.7e96d2d9.chunk.js"
+    "revision": "8944b2d7319575adf4f2",
+    "url": "/static/js/main.a2a3e92e.chunk.js"
   },
   {
     "revision": "540bb2e31db7f83879a2",
